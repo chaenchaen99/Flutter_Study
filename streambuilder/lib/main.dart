80a6d05ff4f2903155ba635b0b93f5066f64ffa6
chaeyeon <chaenjung.99@gmail.com>
2024-03-18 20:52:12 +0900

    wc) streambuilder로 스톱워치 구현

--- streambuilder/lib/main.dart
+++ streambuilder/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:streambuilder/examples/ex_1.dart';
+import 'package:streambuilder/examples/ex_2.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,7 +18,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const ClockScreen(),
+      home: const StopWatchScreen(),
     );
   }
 }
