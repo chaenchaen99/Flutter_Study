12b7354ba65e4d0bb8c8f4e4c06b366197948fa1
chaeyeon <chaenjung.99@gmail.com>
2024-04-03 20:43:29 +0900

    stopwatch 예제 bloc 패턴 적용 완료

--- bloc_stopwatch_example/lib/bloc/stopwatch_bloc.dart
+++ bloc_stopwatch_example/lib/bloc/stopwatch_bloc.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 
+import 'package:bloc_stopwatch_example/utils.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 part 'stopwatch_event.dart';
@@ -21,27 +22,37 @@ class StopwatchBloc extends Bloc<StopwatchEvent, StopwatchState> {
 
   void _onStarted(StopwatchStarted event, Emitter<StopwatchState> emit) {
     _stopwatch.start();
+    emit(state.copyWith(isRunning: true));
+    _tickTimer();
   }
 
   void _onStopped(StopwatchStopped event, Emitter<StopwatchState> emit) {
-    print('onStop');
+    _stopwatch.stop();
+    emit(state.copyWith(isRunning: false));
   }
 
   void _onReset(StopwatchReset event, Emitter<StopwatchState> emit) {
-    print('onReset');
+    _stopwatch.reset();
+    emit(state.copyWith(elapsedTime: 0, laps: const []));
   }
 
   void _onTicked(StopwatchTicked event, Emitter<StopwatchState> emit) {
-    print('onTick');
+    emit(state.copyWith(elapsedTime: event.elapsedTime));
   }
 
   void _onLapRecorded(StopwatchRecorded event, Emitter<StopwatchState> emit) {
-    print('onRecorded');
+   final String lapTime = formatElapsedTime(state.elapsedTime);
+   final List<String> updatedLaps = List<String>.from(state.laps)..add(lapTime);
+   emit(state.copyWith(laps: updatedLaps));
   }
 
   void _tickTimer(){
     Timer.periodic(_tick, (timer) {
-      if(!_stopwatch.isRunning){}else {}
+      if(!_stopwatch.isRunning){
+        timer.cancel();
+      }else {
+        add(StopwatchTicked(elapsedTime: _stopwatch.elapsedMilliseconds));
+      }
     });
   }
 }
