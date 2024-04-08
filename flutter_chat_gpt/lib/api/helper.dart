afed260cde40bd832db4710b9afa4a8e872ffbcb
chaeyeon <chaenjung.99@gmail.com>
2024-04-08 21:39:22 +0900

    hive를 활용한 로컬 데이터 설정

---
+++ flutter_chat_gpt/lib/api/helper.dart
@@ -0,0 +1,18 @@
+
+
+import 'package:flutter_chat_gpt/api/model/completion.dart';
+import 'package:flutter_chat_gpt/hive_model/message_item_model.dart';
+import 'package:flutter_chat_gpt/hive_model/message_role_model.dart';
+
+extension MessageItemListX on List<MessageItemModel> {
+  CompletionRequest toCompletionRequest() {
+    return CompletionRequest(
+      'gpt-3.5-turbo',
+      map((e) => MessageModel(
+          e.role == MessageRoleModel.ai ? 'assistant' : 'user',
+          e.message
+      ).toJson()
+      ).toList(),
+    );
+  }
+}
