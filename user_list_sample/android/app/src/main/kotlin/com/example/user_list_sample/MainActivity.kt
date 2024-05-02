3d4fcc5a83367656f0f305f3257d0c0a6bd2cc01
chaeyeon <chaenjung.99@gmail.com>
2024-05-02 21:28:32 +0900

    상태관리 공부

---
+++ user_list_sample/android/app/src/main/kotlin/com/example/user_list_sample/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.user_list_sample
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ user_list_sample/lib/src/model/user_info_result.dart
@@ -0,0 +1,57 @@
+import 'package:equatable/equatable.dart';
+
+class UserInfoResult extends Equatable {
+  final int currentPage;
+  final List<UserInfo> userInfoList;
+
+  const UserInfoResult({
+    required this.currentPage,
+    required this.userInfoList,
+  });
+
+  factory UserInfoResult.fromJson(Map<String, dynamic> json) {
+    return UserInfoResult(
+        currentPage: (json['info']['page'] as int) + 1,
+        userInfoList: json['results']
+            .map<UserInfo>((item) => UserInfo.fromJson(item))
+            .toList());
+  }
+
+  UserInfoResult copyWithFromJson(Map<String, dynamic> json) {
+    return UserInfoResult(
+        currentPage: (json['info']['page'] as int) + 1,
+        userInfoList: userInfoList
+          ..addAll(
+            json['results'],
+          ));
+  }
+
+  @override
+  List<Object?> get props => throw UnimplementedError();
+}
+
+class UserInfo extends Equatable {
+  final String profileImage;
+  final String name;
+  final String email;
+  final String phone;
+
+  const UserInfo({
+    required this.profileImage,
+    required this.name,
+    required this.email,
+    required this.phone,
+  });
+
+  factory UserInfo.fromJson(Map<String, dynamic> json) {
+    return UserInfo(
+      profileImage: json['picture']['medium'],
+      name: json['name']['first'] + json['name']['last'],
+      email: json['email'],
+      phone: json['phone'],
+    );
+  }
+
+  @override
+  List<Object?> get props => throw UnimplementedError();
+}
